62d1570a668f835e0a049bd2bc3814c23ae0fb10
Oleksandr Kosenko <d1izel@ukr.net>
2024-04-11 22:29:20 +0300

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,88 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import React, { useMemo, useState } from 'react';
+import { TodosList } from './components/TodosList';
+import { TodosFooter } from './components/TodosFooter';
+import { ErrorNotification } from './components/ErrorNotification';
+import { TodoHeader } from './components/TodoHeader';
+import { useDispatch, useSelector } from './providers/TodosContext';
+import { filterTodos } from './helpers/filterTodos';
+import { deleteTodo } from './api/todos';
+import { useError } from './hooks/useError';
+import { Errors, Filters } from './types';
+import { ActionType } from './types/ActionType';
 
 export const App: React.FC = () => {
+  const { todos, inProcess, isError, errorMessage, updateTodos } =
+    useSelector();
+
+  const dispatch = useDispatch();
+  const { setError } = useError();
+
+  const [filterBy, setFilterBy] = useState<Filters>(Filters.All);
+
+  const preparedTodos = useMemo(() => {
+    return filterTodos(todos, filterBy);
+  }, [filterBy, todos]);
+
+  const completedTodos = useMemo(() => {
+    return todos.filter(({ completed }) => completed);
+  }, [todos]);
+
+  const activeTodosLength = todos.length - completedTodos.length;
+
+  const isSomeActive = todos.some(({ completed }) => !completed);
+
+  const handleDeleteCompleted = () => {
+    const completedIds = completedTodos.map(({ id }) => id);
+
+    dispatch({
+      type: ActionType.SetInProcess,
+      payload: [...inProcess, ...completedIds],
+    });
+    Promise.all(completedIds.map(id => deleteTodo(id)))
+      .then(() => {
+        updateTodos();
+      })
+      .catch(() => setError(Errors.UnableDelete))
+      .finally(() => {
+        dispatch({
+          type: ActionType.SetInProcess,
+          payload: [],
+        });
+      });
+  };
+
+  const handleCloseNotification = () => {
+    dispatch({
+      type: ActionType.SetError,
+      payload: { isError: false },
+    });
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader isSomeActive={isSomeActive} />
+
+        <TodosList todos={preparedTodos} />
+
+        {!!todos.length && (
+          <TodosFooter
+            filterBy={filterBy}
+            activeTodosLength={activeTodosLength}
+            completedTodosLength={completedTodos.length}
+            onFilterChange={setFilterBy}
+            onDeleteCompleted={handleDeleteCompleted}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        isHidden={!isError}
+        message={errorMessage}
+        onClose={handleCloseNotification}
+      />
     </div>
   );
 };
